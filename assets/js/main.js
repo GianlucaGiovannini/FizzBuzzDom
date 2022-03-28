af28df37c5e91b4739bd306c7d14784441c3e4bb
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-28 16:41:33 +0200

    Code Review

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,21 @@ for (let i = 1; i <= 100; i++) {
     if ((i % 3 == 0) && (i % 5 == 0)) {
         print = "FizzBuzz";
         color = "bg_red"
+        console.log("FizzBuzz")
     } else if (i % 3 == 0) {
         print = "Fizz";
         color = "bg_green"
+        console.log("Fizz")
     } else if (i % 5 == 0) {
         print = "Buzz";
         color = "bg_yellow"
+        console.log("Buzz")
     } else {
         print = i;
         color = "bg_blue"
+        console.log(i)
     }
 
-    console.log(color)
     const markupContainer = `
      <div class="number ${color} d-flex justify-content-center align-items-center">
         <div>${print}</div>
