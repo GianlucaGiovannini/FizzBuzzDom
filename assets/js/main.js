171c6087ce94256a52f8184283390fd27acf7f82
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-28 16:39:47 +0200

    last Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,28 @@
-/* Scrivi un programma che stampi in console in numeri da 1 a 100 */
 const numbersElement = document.querySelector(".container")
-const backgroundElement = document.querySelector(".number")
+let print;
+let color;
 
 for (let i = 1; i <= 100; i++) {
-    let print;
 
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        print = "FizzBuzz";
+        color = "bg_red"
+    } else if (i % 3 == 0) {
+        print = "Fizz";
+        color = "bg_green"
+    } else if (i % 5 == 0) {
+        print = "Buzz";
+        color = "bg_yellow"
+    } else {
+        print = i;
+        color = "bg_blue"
+    }
+
+    console.log(color)
     const markupContainer = `
-     <div class="number d-flex justify-content-center align-items-center">
+     <div class="number ${color} d-flex justify-content-center align-items-center">
         <div>${print}</div>
      </div>
      `
-
-    if ((i % 3 == 0) && (i % 5 == 0)) {
-
-        print = "FizzBuzz";
-        numbersElement.insertAdjacentHTML("beforeend", markupContainer);
-        backgroundElement.classList.add("bg_red");
-        console.log("FizzBuzz");
-    } else if (i % 3 == 0) {
-        print = "Fizz";
-        numbersElement.insertAdjacentHTML("beforeend", markupContainer);
-        console.log("Fizz");
-        backgroundElement.classList.add("bg_green");
-    } else if (i % 5 == 0) {
-        print = "Buzz";
-        numbersElement.insertAdjacentHTML("beforeend", markupContainer);
-        backgroundElement.classList.add("bg_yellow");
-        console.log("Buzz");
-    } else {
-        print = i;
-        numbersElement.insertAdjacentHTML("beforeend", markupContainer);
-        backgroundElement.classList.add("bg_blue");
-        console.log(i);
-    }
-
-    // numbersElement.insertAdjacentHTML("beforeend", markupContainer)
+    numbersElement.insertAdjacentHTML("beforeend", markupContainer);
 }
