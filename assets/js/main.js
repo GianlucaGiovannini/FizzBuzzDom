17dfd07c4ba3febe25a5ce53b80091f8c40eab76
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-28 15:04:31 +0200

    Code Review

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,25 @@
 /* Scrivi un programma che stampi in console in numeri da 1 a 100 */
-const divContainerElement = document.querySelector(".container")
+const numbersElement = document.querySelector(".container")
 
 for (let i = 1; i <= 100; i++) {
 
-    const markupContainer = `
-    <div class="number d-flex justify-content-center align-items-center">
-    <div>${i}</div>
-    </div>
-    `
+    /* const markupContainer = `
+     <div class="number d-flex justify-content-center align-items-center">
+     <div>${i}</div>
+     </div>
+     ` */
     if ((i % 3 == 0) && (i % 5 == 0)) {
+
         console.log("FizzBuzz")
     } else if (i % 3 == 0) {
+
         console.log("Fizz")
     } else if (i % 5 == 0) {
+
         console.log("Buzz")
     } else {
         console.log(i)
     }
 
-
+    // numbersElement.insertAdjacentHTML("beforeend", markupContainer)
 }
